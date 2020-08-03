7969cd2467c94c98461dbbe2669daf37b04543c6
haohao6683 <852721803@qq.com>
2020-08-04 00:44:20 +0800

    [react-counter][Draymond] Refactor: Refactor the reducers named counter.js

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,7 +1,11 @@
 const counter = (state = {}, action = {}) => {
     switch(action.type){
-        default : return state; 
+        case 'INCREASE' : 
+            return Object.assign({}, state, {count: state.count + 1});
+        case 'DECREASE' :
+            return Object.assign({}, state, {count: state.count - 1});
     }
+    return state;
 }
 
 export default counter;
