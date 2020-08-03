a36d661b5d14e3969adc789f3e2fe8660cbfc89c
haohao6683 <852721803@qq.com>
2020-08-03 16:20:15 +0800

    [react-counter][Draymond] Feature: Adding an input label to implement the CountersGroup

--- src/components/CountersGroup/index.jsx
+++ src/components/CountersGroup/index.jsx
@@ -4,12 +4,24 @@ import Counter from '../Counter';
 class CountersGroup extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            number : 0
+        };
+    }
+
+    changeEvent(e){
+        {
+            this.setState({
+                number: parseInt(e.target.value)
+            });
+        }
     }
 
     render(){
         return(
             <div>
-                {new Array(5).fill(0).map(() => <Counter />)}
+                number of counters: <input id="numbers" type="text" onChange={(e)=>this.changeEvent(e)} />
+                {new Array(this.state.number).fill(0).map((value, index) => <Counter key={index} />)}
             </div>
         );
     }
