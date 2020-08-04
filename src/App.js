a19e1911e188c00f544e1f44b6aefaedb8e4a36f
haohao6683 <852721803@qq.com>
2020-08-05 03:47:18 +0800

    [react-counter][Draymond] Refactor: refactor the App() method of App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Provider} from 'react-redux';
-import store from './store/store'
 import CountersGroup from './components/CountersGroup';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Provider store = {store}>
           <CountersGroup />
-        </Provider>
       </header>
     </div>
   );
