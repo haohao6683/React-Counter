555f86b6645f641565c993d975165a1829868144
haohao6683 <852721803@qq.com>
2020-08-05 03:51:03 +0800

    [react-counter][Draymond] Refactor: refactor the global index.js.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store = {store}>
-        <CountersGroup />
+        <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
